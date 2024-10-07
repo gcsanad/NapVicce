1f27ce3b9bbd3eb870bacb9bac6e29c23853cd6c
you@example.com <you@example.com>
2024-10-07 13:53:03 +0200

    Add project files.

---
+++ nepVicce/Controllers/NapVicceController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace nepVicce.Controllers
+{
+    public class NapVicceController : Controller
+    {
+        Dictionary<string, string> viccz = new Dictionary<string, string>() {
+
+        {"Monday", "funny" },
+        {"Tuesday", "xd" },
+        {"Wednesday", "funny" },
+        {"Thursday", "funny" },
+        {"Friday", "funny" },
+        {"Saturday", "funny" },
+        {"Sunday", "funny" }
+            
+        };
+        [HttpGet]
+        [Route("[controller]")]
+        
+        public string Get()
+        {
+            //List<string> viccz = new() {"funny", "xd", "na uh", "viccike", "nagyon vicces", "joke", "mark hamil" };
+            return $"{viccz.Where(x => x.Key == DateTime.Now.DayOfWeek.ToString()).First().Key} : {viccz.Where(x => x.Key == DateTime.Now.DayOfWeek.ToString()).First().Value}" ;
+        }
+    }
+}
